cd896c5030c4e57550215008b569984dca7246ed
João Pedro Alves <joaoalves3470@gmail.com>
2019-09-03 10:37:11 -0300

    Add files via upload

---
+++ Mundo1/ex014.py
@@ -0,0 +1,2 @@
+n1=float(input('informe a temperatura em C: '))
+print(f'A temperatura de {n1}C equivale a temperatura de {(n1*9/5)+32:.1f}F')
---
+++ Mundo1/ex015.py
@@ -0,0 +1,5 @@
+n1=float(input('Quantos dias o carro foi alugado? '))
+n2=float(input('Quantos Km rodados? '))
+n3=n1*60
+n4=n2*0.15
+print(f'O total a pagar e de R${n3+n4:.2f}')
